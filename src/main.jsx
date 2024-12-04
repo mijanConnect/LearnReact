3711dd8fd961c9b87f2feb9a4a63e96fe4e6429b
Mijanur Rahman <mijanur5rahman@gmail.com>
2024-12-04 20:48:54 +0600

    Hook useRef using for Input element access

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import HookUseRefInnerHTMLcng from './pages/HookUseRefInnerHTMLcng'
+import HookUseRefInputElement from './pages/HookUseRefInputElement'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HookUseRefInnerHTMLcng/>
+    <HookUseRefInputElement/>
   </StrictMode>,
 ) 
