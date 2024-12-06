cc3da6f0c1ab74a0d8441d2cee782b89d16e3a07
Mijanur Rahman <mijanur5rahman@gmail.com>
2024-12-06 10:53:38 +0600

    Object change using usestate

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import HookUseStateCounter from './pages/HookUseStateCounter';
+import HookUseStateObject from './pages/HookUseStateObject';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HookUseStateCounter/>
+    <HookUseStateObject/>
   </StrictMode>,
 )
