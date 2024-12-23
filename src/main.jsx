16aab18a36644231555875866047216ed33191ee
Mijanur Rahman <mijanur5rahman@gmail.com>
2024-12-23 22:09:53 +0600

    Hook useState structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'milligram/dist/milligram.css'
-import HookUseStateForm from './pages/HookUseStateForm';
+import HookUseEffect from './pages/HookUseEffect';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HookUseStateForm/>
-  </StrictMode>,
+
+    <HookUseEffect/>
+
 )
